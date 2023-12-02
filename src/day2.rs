d60c7cf96adf8b3e7d131181b84b2e8ece657e84
Davide Rizzo <sorcio@gmail.com>
2023-12-02 11:58:31 +0100

    day 2 part 2

--- src/day2.rs
+++ src/day2.rs
@@ -17,6 +17,28 @@ impl Colors {
     fn is_within_limit(&self, limit: &Colors) -> bool {
         self.r <= limit.r && self.g <= limit.g && self.b <= limit.b
     }
+
+    fn power(&self) -> u32 {
+        self.r
+            .checked_mul(self.g)
+            .unwrap()
+            .checked_mul(self.b)
+            .unwrap()
+    }
+}
+
+trait ColorsLowerBound {
+    fn find_lower_bound(&self) -> Option<Colors>;
+}
+
+impl<T: std::ops::Deref<Target = [Colors]>> ColorsLowerBound for T {
+    fn find_lower_bound(&self) -> Option<Colors> {
+        Some(Colors {
+            r: self.iter().map(|c| c.r).max()?,
+            g: self.iter().map(|c| c.g).max()?,
+            b: self.iter().map(|c| c.b).max()?,
+        })
+    }
 }
 
 #[aoc_generator(day2)]
@@ -89,8 +111,11 @@ fn part1(input: &[Game]) -> u32 {
 }
 
 #[aoc(day2, part2)]
-fn part2(input: &[Game]) -> String {
-    todo!()
+fn part2(input: &[Game]) -> u32 {
+    input
+        .iter()
+        .map(|game| game.sets.find_lower_bound().unwrap().power())
+        .sum()
 }
 
 #[cfg(test)]
@@ -111,8 +136,17 @@ mod tests {
         );
     }
 
-    // #[test]
-    // fn part2_example() {
-    //     assert_eq!(part2(&parse("<EXAMPLE>")), "<RESULT>");
-    // }
+    #[test]
+    fn part2_example() {
+        assert_eq!(
+            part2(&parse(
+                "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+        Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+        Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+        Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+        Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+            )),
+            2286
+        );
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-mod day2;
 mod day1;
+mod day2;
 use aoc_runner_derive::aoc_lib;
 
-aoc_lib!{ year = 2023 }
+aoc_lib! { year = 2023 }
