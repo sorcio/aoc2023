4343b3d296cb027af20e17098cb52e5eeca6bb5c
Davide Rizzo <sorcio@gmail.com>
2023-12-18 16:14:15 +0100

    testing: unindent also when parser is None

--- src/testing.rs
+++ src/testing.rs
@@ -14,29 +14,40 @@ where
     pub(crate) result: &'static O,
     pub(crate) marker: PhantomData<I>,
 }
+pub(crate) trait Unindentable {
+    type Output: Borrow<Self>;
+    fn unindent(&self) -> Self::Output;
+}
+
+impl Unindentable for str {
+    type Output = String;
+    fn unindent(&self) -> String {
+        unindent::unindent(self)
+    }
+}
+
+impl Unindentable for [u8] {
+    type Output = Vec<u8>;
+    fn unindent(&self) -> Vec<u8> {
+        unindent::unindent_bytes(self)
+    }
+}
 
 trait ParserOrNone<'s, T: ?Sized> {
     type Parsed;
     fn parse(self, input: &'s T) -> Self::Parsed;
 }
 
-impl<F: FnOnce(&str) -> I, I> ParserOrNone<'_, str> for F {
+impl<'s, F: FnOnce(&T) -> I, T: Unindentable + ?Sized + 's, I> ParserOrNone<'s, T> for F {
     type Parsed = I;
-    fn parse(self, input: &str) -> I {
-        self(&unindent::unindent(input))
+    fn parse(self, input: &'s T) -> Self::Parsed {
+        self(input)
     }
 }
 
-impl<F: FnOnce(&[u8]) -> I, I> ParserOrNone<'_, [u8]> for F {
-    type Parsed = I;
-    fn parse(self, input: &[u8]) -> I {
-        self(&unindent::unindent_bytes(input))
-    }
-}
-
-impl<'s, T: ?Sized + 's> ParserOrNone<'s, T> for Option<()> {
+impl<'s, T: Unindentable + ?Sized + 's> ParserOrNone<'s, T> for Option<()> {
     type Parsed = &'s T;
-    fn parse(self, input: &'s T) -> &'s T {
+    fn parse(self, input: &'s T) -> Self::Parsed {
         match self {
             Some(_) => panic!("parser should be a function or None"),
             None => input,
@@ -78,17 +89,18 @@ macro_rules! example_tests {
             $(
                 #[test]
                 fn $solver_name() {
-                    use $crate::testing::CorrectResultTest;
+                    use std::borrow::Borrow;
+                    use $crate::testing::{CorrectResultTest, Unindentable};
                     #[allow(unused_variables)]
-                    let example_data = $example_data;
+                    let example_data = $example_data.unindent();
                     $(
-                        let example_data = $per_part_example_data;
+                        let example_data = $per_part_example_data.unindent();
                     )?
                     {
                     CorrectResultTest {
                         parser: $parser,
                         solver: super::$solver_name,
-                        example: example_data,
+                        example: example_data.borrow(),
                         result: &$result,
                         marker: std::marker::PhantomData,
                     }.test();
@@ -120,13 +132,14 @@ macro_rules! known_input_tests {
             $(
                 #[test]
                 fn $solver_name() {
-                    use $crate::testing::CorrectResultTest;
-                    let example_data = $input;
+                    use std::borrow::Borrow;
+                    use $crate::testing::{CorrectResultTest, Unindentable};
+                    let example_data = $input.unindent();
                     {
                     CorrectResultTest {
                         parser: $parser,
                         solver: super::$solver_name,
-                        example: example_data,
+                        example: example_data.borrow(),
                         result: &$result,
                         marker: std::marker::PhantomData,
                     }.test();
